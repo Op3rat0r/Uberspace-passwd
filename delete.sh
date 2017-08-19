7d94ed6a3d24d9759cb660a5e9146608b7ee255b
Daniel <service@justit.eu>
2017-08-19 14:24:11 +0200

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,3 +1,3 @@
-if [ "/var/www/virtual/webfolder/passwd.subdomain.de/access.json" ]; then
-	rm -f /var/www/webfolder/kayos/passwd.subdomain.de/access.json
+if [ "/var/www/virtual/uberspaceusername/passwd.subdomain.de/access.json" ]; then
+	rm -f /var/www/uberspaceusername/virtual/passwd.subdomain.de/access.json
 fi
